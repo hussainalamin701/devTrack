1dc4273e7a8efa82a6e3e62e223cedc61b54a879
Alamin Hussain <140621473+hussainalamin701@users.noreply.github.com>
2026-08-20 14:36:31 +0100

    implemented more main content UI

--- components/layout/Dashboard.tsx
+++ components/layout/Dashboard.tsx
@@ -2,8 +2,18 @@
 
 export default function Dashboard () {
     return(
-        <div>
-            <p>Dashie</p>
+        <div className="flex-1 p-10 space-y-6">
+            <div className="grid grid-cols-4 gap-6">
+                <div className="card">Projects</div>
+                <div className="card">Tasks</div>
+                <div className="card">Time</div>
+                <div className="card">Progress</div>
+            </div>
+
+            <div className="grid grid-cols-2 gap-6">
+                <div className="card">Today's Tasks</div>
+                <div className="card">Recent Projects</div>
+            </div>
         </div>
     )
 }
